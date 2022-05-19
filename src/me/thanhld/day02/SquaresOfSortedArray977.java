2d55c4fa78b0cbd23cbbee8fdb26a6df77a62aa8
Le Duy Thanh <thanhld@softdreams.vn>
2022-05-19 22:24:32 +0700

    day2 - Squares of sorted array - 977

--- src/me/thanhld/day02/SquaresOfSortedArray977.java
+++ src/me/thanhld/day02/SquaresOfSortedArray977.java
@@ -1,16 +1,32 @@
 package me.thanhld.day02;
 
+import java.util.Arrays;
+
 public class SquaresOfSortedArray977 {
-//    public static int[] sortedSquares(int[] nums) {
-//        for (int num : nums
-//        ) {
-//            num = Math.abs(num);
-//        }
-//    }
+    public static int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int start = 0;
+        int end = nums.length - 1;
+        int index = end;
+        while (start <= end) {
+            int n1 = nums[start] * nums[start];
+            int n2 = nums[end] * nums[end];
+            if (n1 > n2) {
+                result[index] = n1;
+                start++;
+            } else {
+                result[index] = n2;
+                end--;
+            }
+            index--;
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
         int[] input = new int[]{-4, -1, 0, 3, 10};
-        int[] output = null;
-        System.out.println(output);
+        int[] output = sortedSquares(input);
+        System.out.println(Arrays.toString(output));
     }
 }
