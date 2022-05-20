332fa48abd56e8c74cbbe364478c13fe949d43f1
Le Duy Thanh <thanhld@softdreams.vn>
2022-05-20 09:40:53 +0700

    solved rotate-array-189

--- src/me/thanhld/day02/RotateArray189.java
+++ src/me/thanhld/day02/RotateArray189.java
@@ -6,27 +6,47 @@ import java.util.stream.Collectors;
 
 public class RotateArray189 {
     public static void rotate(int[] nums, int k) {
-        List<Integer> _nums = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int[] copy = nums.clone();
         if (k % nums.length != 0) {
             int minK = k % nums.length;
-            List<Integer> _sub1 = _nums.subList(0, nums.length - minK);
-            List<Integer> _sub2 = _nums.subList(nums.length - minK, nums.length);
-            _sub2.addAll(_sub1);
-
-            for (int i = 0; i < k; i++) {
-                int temp = nums[i];
-                int tempik = nums[i + k];
-                nums[i] = nums[i + k];
-                nums[i+1] = temp;
-                nums[i-k] =
+            for (int i = 0; i < copy.length; i++) {
+                if (i < minK) {
+                    nums[i] = copy[copy.length - minK + i];
+                } else {
+                    nums[i] = copy[i - minK];
+                }
             }
+            System.out.println(Arrays.toString(nums));
         }
+    }
+
+    public static void anotherRotateSolution(int[] nums, int k) {
+        k = k % nums.length;
+        r(nums, 0, nums.length - 1);
+        r(nums, 0, k - 1);
+        r(nums, k, nums.length - 1);
+
 
     }
 
+    public static void r(int[] n, int s, int e) {
+        int a = s, b = e;
+        int t = -1;
+
+        // revert array
+        while (a < b) {
+            t = n[a];
+            n[a] = n[b];
+            n[b] = t;
+            a++;
+            b--;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int step = 9;
-        rotate(input, step);
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int step = 3;
+//        rotate(input, step);
+        anotherRotateSolution(input, step);
     }
 }
